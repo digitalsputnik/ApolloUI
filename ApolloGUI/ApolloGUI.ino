fe5df2d105c7393a6c36870b22cbc4f99e64a4b5
KKallas <kaspar@digitalsputnik.com>
2022-06-06 18:45:57 +0300

    initial encoder

--- ApolloGUI/ApolloGUI.ino
+++ ApolloGUI/ApolloGUI.ino
@@ -5,15 +5,32 @@
 #define LGFX_USE_V1         // set to use new version of library
 
 #include <LovyanGFX.hpp>    // main library
-//#include <Adafruit_ImageReader.h>
+#include <FastLED.h>
+#include <Wire.h>
+
+
+//led control
+#define NUM_LEDS 6
+#define LED_DATA_PIN 4
 
 static LGFX lcd;            // declare display variable
-static LGFX_Sprite sprite(&lcd);
+
+hw_timer_t * timer = NULL;
+
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+
+
+
 int count=0;
 bool state=0;
 // Variables for touch x,y
 static int32_t x,y;
 int X,Y;
+
+volatile bool tick_update=false;
+int intensity_val, prev_intensity_val;
 
 //Adafruit_Image img;
 
@@ -21,9 +38,10 @@ class eButton {
   private:
     int Xpos,Ypos,Width,Height;
     LGFX *_lcd_ptr;
-    bool _update = true;
+    
     String Type, Val;
   public:
+    bool _update = true;
     eButton(int _x, int _y, int _width, int _height, LGFX *_inLcd, String _type, String _val) {
       Xpos = _x;
       Ypos = _y;
@@ -70,7 +88,12 @@ class eButton {
           lcd.drawString(Type, 30, Ypos+10);
           lcd.setTextDatum(textdatum_t::top_right);
           lcd.setTextColor(_lcd_ptr->color888(79,79,79), _lcd_ptr->color888(46,46,46));
-          lcd.drawString(Val, 290, Ypos+10);          
+          if(Type == "Intensity") {
+            lcd.drawString(String(intensity_val)+"%", 290, Ypos+10);
+          }
+          else {
+            lcd.drawString(Val, 290, Ypos+10);          
+          }
         }
         _update=false;
       }
@@ -78,7 +101,7 @@ class eButton {
 };
 
 eButton GroupSelector(0,0,320,54,&lcd,"G","Apollo0000");
-eButton propIntensity(10,64,300,40,&lcd,"Intenisty","80%");
+eButton propIntensity(10,64,300,40,&lcd,"Intensity","80%");
 eButton propSaturation(10,114,300,40,&lcd,"Saturation","0%");
 eButton propHue(10,164,300,40,&lcd,"Hue","0%");
 eButton propTemperature(10,214,300,40,&lcd,"Temperature","5600K");
@@ -86,8 +109,21 @@ eButton propFx(10,264,300,40,&lcd,"Fx","0%");
 eButton propViewers(10,314,300,40,&lcd,"Manage viewers","");
 eButton propButton(160,430,150,40,&lcd,"B","Setup");
 
+
+
+void tick5ms() {
+  tick_update=true;
+  }
+
 void setup(void)
 {
+    
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &tick5ms, true);
+  timerAlarmWrite(timer, 100000, true);
+  timerAlarmEnable(timer);
+  
+  Wire.begin(25,5);
   Serial.begin(115200);
   lcd.init();
   lcd.setColorDepth(24);
@@ -95,11 +131,78 @@ void setup(void)
   // Setting display to landscape
   //if (lcd.width() < lcd.height()) lcd.setRotation(lcd.getRotation() ^ 1);
 
+  FastLED.addLeds<WS2812, LED_DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+
+  leds[0] = CRGB::Cyan;
+  leds[1] = CRGB::Cyan;
+  leds[2] = CRGB::Cyan;
+  leds[3] = CRGB::Cyan;
+  leds[4] = CRGB::Green;
+  leds[5] = CRGB::Magenta;
+  FastLED.show();
+
+  //i2cScan();
+
+  getEncoder();
+  prev_intensity_val = intensity_val;
+
+}
+
+void i2cScan() {
+  byte error, address;
+  int nDevices;
+  Serial.println("Scanning...");
+  nDevices = 0;
+  for(address = 1; address < 127; address++ ) {
+    Wire.beginTransmission(address);
+    error = Wire.endTransmission();
+    if (error == 0) {
+      Serial.print("I2C device found at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+      nDevices++;
+    }
+    else if (error==4) {
+      Serial.print("Unknow error at address 0x");
+      if (address<16) {
+        Serial.print("0");
+      }
+      Serial.println(address,HEX);
+    }    
+  }
+
+
+}
+
+void getEncoder() {
+  //connect to encoder
+  uint8_t data[2];
+  Wire.beginTransmission(0x40);
+  Wire.write(0x10);
+  Wire.endTransmission();
+
+  //read encoder value
+  Wire.requestFrom(0x40, 2);
+  for (int i = 0; i<2; i++) {
+    data[i] = Wire.read();
+  }
+
+  intensity_val = (signed short int)((data[0]) | (data[1]) << 8);
+
+  if(prev_intensity_val!=intensity_val) {
+    propIntensity._update=true;
+    prev_intensity_val=intensity_val;
+  }
 }
 
 void loop()
 {
-
+  if(tick_update==true) {
+    tick_update=false;
+    getEncoder();
+  }
   GroupSelector.reDraw();
   propIntensity.reDraw();
   propSaturation.reDraw();
